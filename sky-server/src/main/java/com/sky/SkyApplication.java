acf04f556079cdd48ce2fd9f72d60e85416fd3b4
logey <logey1314@gmail.com>
2025-08-15 14:21:07 +0800

    定时任务开发

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -4,14 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @EnableCaching
+@EnableScheduling
 @Slf4j
 public class SkyApplication {
-    //todo 管理端套餐接口没有实现 课程代码也没有
+
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,20 @@
+package com.sky.task;
+
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class OrderTask {
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Scheduled(cron = "0 * * * * ?")
+    public void timeoutOrder(){
+        log.info("定时处理超时订单");
+
+    }
+}
